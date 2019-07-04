1af306abb9c491f0ce2bfa37081a10d409c125fe
aiueta <35910535+aiueta@users.noreply.github.com>
2019-07-04 14:43:51 +0900

    Update post.js

--- post.js
+++ post.js
@@ -3,7 +3,7 @@ $(function () {
     var url = 'https://slack.com/api/chat.postMessage';
         var data = {
             token: 'xoxp-645645449623-643295357428-686550594535-94a2955170f0724bdf86b2ba51d73876',
-            channel: '#入退室記録',
+            channel: '#general',
             username: 'oreno-bot',
             text: 'Hello Slack!'
         };
